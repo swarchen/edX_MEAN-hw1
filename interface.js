3838a85c466d8a42364f04ca79196bb6dbd101e5
jhaocheng <jhaocheng.wu@gmail.com>
2015-11-17 22:40:33 +0800

    chapter1

---
+++ interface.js
@@ -0,0 +1,23 @@
+/*
+ *  Inserts "doc" into the collection "movies".
+ */
+exports.insert = function(db, doc, callback) {
+  // TODO: implement
+  db.collection('movies').insert(doc);
+  callback(null);
+};
+
+/*
+ *  Finds all documents in the "movies" collection
+ *  whose "director" field equals the given director,
+ *  ordered by the movie's "title" field. See
+ *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
+ */
+exports.byDirector = function(db, director, callback) {
+  // TODO: implement
+  db.collection('movies').find({director: director}).sort({title : 1}).toArray(function(error, results){
+            if(error) callback(error);
+            else callback(null, results);
+        });
+  
+};
